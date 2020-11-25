ae462424b333fb8b9676f028f0feaad67e8220af
john <jgh2102@gmail.com>
2020-11-26 07:21:35 +0900

    build and npm package modified

---
+++ appFrontend/build/precache-manifest.bbec43bd9b426f2d0b5a24d696ad9d7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018118af47ee9be0ae902dca2bcd9e6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2febe54a17ecfc97cce",
+    "url": "/static/css/main.2db320d4.chunk.css"
+  },
+  {
+    "revision": "b8970eda38d5923479de",
+    "url": "/static/js/2.a86bb985.chunk.js"
+  },
+  {
+    "revision": "59be4b762375662573505f74a0721fea",
+    "url": "/static/js/2.a86bb985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2febe54a17ecfc97cce",
+    "url": "/static/js/main.36dc54b1.chunk.js"
+  },
+  {
+    "revision": "4cbfa379d4e7a04966b5",
+    "url": "/static/js/runtime-main.9b24350c.js"
+  },
+  {
+    "revision": "4999058019213651b8b420a092a2030e",
+    "url": "/static/media/leaf-clipart-12-transparent.49990580.png"
+  }
+]);
